48c8ece40e13235299e395fcdd09aa4d81c717c5
EZreal-Map <73785664+EZreal-Map@users.noreply.github.com>
2022-09-14 22:27:41 +0800

    Delete code/code directory

--- code/code/Utime.m
+++
@@ -1,9 +0,0 @@
-format = 'M月d日 HH:mm';
-infmt = 'M.d H.mm'
-datetime(2004,2,29,14,0,0,'Format',format);
-t=datetime('5.12 23.21','InputFormat',infmt)
-t.Minute=t.Minute*0.6
-clc;clear
-load('time.mat')
-a = ans
-a(1,:).Time
